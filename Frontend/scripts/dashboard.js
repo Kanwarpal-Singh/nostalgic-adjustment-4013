8e324de038f2de4b6a4ca58268c29480a243e136
keertiPawar8 <keertipawar8@gmail.com>
2023-06-17 22:51:45 +0530

    dashboard updated

--- Frontend/scripts/dashboard.js
+++ Frontend/scripts/dashboard.js
@@ -2,34 +2,118 @@
 
 let data = [
 
+  {
+  "_id": "648c32bf6c3782a189731d43",
+  "title": "The Complete Data Analyst Bootcamp",
+  "course": "Data Analyst",
+  "description": "The course provides the complete preparation you need to become a data analyst.\r\nFill up your resume with in-demand data skills: Python programming, NumPy, pandas, data preparation - data collection, data cleaning, data preprocessing, data visualization; data analysis, data analytics\r\nAcquire a big-picture understanding of the data analyst role.\r\nLearn beginner and advanced Python.\r\nStudy mathematics for Python.\r\nWe will teach you NumPy and pandas, basics, and advanced.",
+  "language": "English",
+  "creatorId": "641c32bf6c3782a189731d43",
+  "creatorName": "dummy",
+  "content": [
     {
-    "_id": "648c32bf6c3782a189731d43",
-    "title": "The Complete Data Analyst Bootcamp",
-    "course": "Data Analyst",
-    "description": "The course provides the complete preparation you need to become a data analyst.\r\nFill up your resume with in-demand data skills: Python programming, NumPy, pandas, data preparation - data collection, data cleaning, data preprocessing, data visualization; data analysis, data analytics\r\nAcquire a big-picture understanding of the data analyst role.\r\nLearn beginner and advanced Python.\r\nStudy mathematics for Python.\r\nWe will teach you NumPy and pandas, basics, and advanced.",
-    "language": "English",
-    "creatorId": "641c32bf6c3782a189731d43",
-    "creatorName": "dummy",
-    "content": [
-      {
-        "videoName": "videos/Dataanalyst-Video-1.m4v",
-        "videoUrl": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/videos/Dataanalyst-Video-1.m4v?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=96bbdcba1657e72b881fff90ed0f4c33f13a9951312f2f7cc7cb23688f6c3cdd&X-Amz-SignedHeaders=host&x-id=GetObject",
-        "postedAt": "2023-06-16T09:59:40.854Z",
-        "shares": 0,
-        "thumbnailName": "images/DataAnalyst.png",
-        "thumbnailURL": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/images/DataAnalyst.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=c08cf26abccc93ff74fe2bbe5d3ccc23c9d8d7af875d7e37b5ccaa438d903e60&X-Amz-SignedHeaders=host&x-id=GetObject",
-        "_id": "648c32bf6c3782a189731d44",
-        "likedBy": [
-          
-        ],
-        "comments": [
-          
-        ]
-      }
-    ]
-  } 
+      "videoName": "videos/Dataanalyst-Video-1.m4v",
+      "videoUrl": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/videos/Dataanalyst-Video-1.m4v?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=96bbdcba1657e72b881fff90ed0f4c33f13a9951312f2f7cc7cb23688f6c3cdd&X-Amz-SignedHeaders=host&x-id=GetObject",
+      "postedAt": "2023-06-16T09:59:40.854Z",
+      "shares": 0,
+      "thumbnailName": "images/DataAnalyst.png",
+      "thumbnailURL": "https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&rs=1&c=1&qlt=95&w=198&h=124",
+      "_id": "648c32bf6c3782a189731d44",
+      "likedBy": [
+        
+      ],
+      "comments": [
+        
+      ]
+    }
+  ]
+} ,
+
+{
+  "_id": "648c32bf6c3782a189731d43",
+  "title": "The Complete Data Analyst Bootcamp",
+  "course": "Data Analyst",
+  "description": "The course provides the complete preparation you need to become a data analyst.\r\nFill up your resume with in-demand data skills: Python programming, NumPy, pandas, data preparation - data collection, data cleaning, data preprocessing, data visualization; data analysis, data analytics\r\nAcquire a big-picture understanding of the data analyst role.\r\nLearn beginner and advanced Python.\r\nStudy mathematics for Python.\r\nWe will teach you NumPy and pandas, basics, and advanced.",
+  "language": "English",
+  "creatorId": "641c32bf6c3782a189731d43",
+  "creatorName": "dummy",
+  "content": [
+    {
+      "videoName": "videos/Dataanalyst-Video-1.m4v",
+      "videoUrl": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/videos/Dataanalyst-Video-1.m4v?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=96bbdcba1657e72b881fff90ed0f4c33f13a9951312f2f7cc7cb23688f6c3cdd&X-Amz-SignedHeaders=host&x-id=GetObject",
+      "postedAt": "2023-06-16T09:59:40.854Z",
+      "shares": 0,
+      "thumbnailName": "images/DataAnalyst.png",
+      "thumbnailURL": "https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&rs=1&c=1&qlt=95&w=198&h=124",
+      "_id": "648c32bf6c3782a189731d44",
+      "likedBy": [
+        
+      ],
+      "comments": [
+        
+      ]
+    }
+  ]
+} ,
+
+
+{
+  "_id": "648c32bf6c3782a189731d43",
+  "title": "The Complete Data Analyst Bootcamp",
+  "course": "Data Analyst",
+  "description": "The course provides the complete preparation you need to become a data analyst.\r\nFill up your resume with in-demand data skills: Python programming, NumPy, pandas, data preparation - data collection, data cleaning, data preprocessing, data visualization; data analysis, data analytics\r\nAcquire a big-picture understanding of the data analyst role.\r\nLearn beginner and advanced Python.\r\nStudy mathematics for Python.\r\nWe will teach you NumPy and pandas, basics, and advanced.",
+  "language": "English",
+  "creatorId": "641c32bf6c3782a189731d43",
+  "creatorName": "dummy",
+  "content": [
+    {
+      "videoName": "videos/Dataanalyst-Video-1.m4v",
+      "videoUrl": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/videos/Dataanalyst-Video-1.m4v?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=96bbdcba1657e72b881fff90ed0f4c33f13a9951312f2f7cc7cb23688f6c3cdd&X-Amz-SignedHeaders=host&x-id=GetObject",
+      "postedAt": "2023-06-16T09:59:40.854Z",
+      "shares": 0,
+      "thumbnailName": "images/DataAnalyst.png",
+      "thumbnailURL": "https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&rs=1&c=1&qlt=95&w=198&h=124",
+      "_id": "648c32bf6c3782a189731d44",
+      "likedBy": [
+        
+      ],
+      "comments": [
+        
+      ]
+    }
+  ]
+} ,
+
+
+{
+  "_id": "648c32bf6c3782a189731d43",
+  "title": "The Complete Data Analyst Bootcamp",
+  "course": "Data Analyst",
+  "description": "The course provides the complete preparation you need to become a data analyst.\r\nFill up your resume with in-demand data skills: Python programming, NumPy, pandas, data preparation - data collection, data cleaning, data preprocessing, data visualization; data analysis, data analytics\r\nAcquire a big-picture understanding of the data analyst role.\r\nLearn beginner and advanced Python.\r\nStudy mathematics for Python.\r\nWe will teach you NumPy and pandas, basics, and advanced.",
+  "language": "English",
+  "creatorId": "641c32bf6c3782a189731d43",
+  "creatorName": "dummy",
+  "content": [
+    {
+      "videoName": "videos/Dataanalyst-Video-1.m4v",
+      "videoUrl": "https://course-craft-videos.s3.ap-south-1.amazonaws.com/videos/Dataanalyst-Video-1.m4v?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATEUVAXTOSYIGSTWW%2F20230616%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230616T100031Z&X-Amz-Expires=432000&X-Amz-Signature=96bbdcba1657e72b881fff90ed0f4c33f13a9951312f2f7cc7cb23688f6c3cdd&X-Amz-SignedHeaders=host&x-id=GetObject",
+      "postedAt": "2023-06-16T09:59:40.854Z",
+      "shares": 0,
+      "thumbnailName": "images/DataAnalyst.png",
+      "thumbnailURL": "https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&rs=1&c=1&qlt=95&w=198&h=124",
+      "_id": "648c32bf6c3782a189731d44",
+      "likedBy": [
+        
+      ],
+      "comments": [
+        
+      ]
+    }
+  ]
+} 
+
 ]
-    
+  
 
 
 
@@ -38,11 +122,11 @@ let data = [
 // window.onload("load",()=>{
 
 
-    // getData()
+  // getData()
 
 
 
- 
+
 
 
 
@@ -64,7 +148,7 @@ let data = [
 //     catch(err){
 //         console.log(err)
 //     }
-   
+ 
 
 // }
 
@@ -87,30 +171,56 @@ data.forEach((el)=>{
 let card = document.createElement("div")
 card.setAttribute("class","display-content")
 
+let imagediv = document.createElement("div")
+imagediv.setAttribute("id","image-container")
 
 
 let image = document.createElement("img");
+image.setAttribute("id","imagedisplay")
 
 image.setAttribute("src",el.content[0].thumbnailURL)
 
 image.addEventListener("click",(e)=>{
-    e.preventDefault()
-    clickevent( el.content[0].videoUrl, el.content[0].videoName)
+  e.preventDefault()
+  clickevent( el.content[0].videoUrl, el.content[0].videoName)
 })
+
+
 
 
 let title = document.createElement("h3")
 
-title.innerText = el.title;
+title.innerText =`Title : ${el.title}` ;
 
 
 let owner = document.createElement("h3")
-owner.innerText = el.creatorName;
+owner.innerText = `Created By : ${el.creatorName}`;
 
 let createdDate  = document.createElement("h3")
-createdDate.innerText = el.content[0].postedAt;
 
 
+
+
+const dateTimeString = el.content[0].postedAt;
+const dateTime = new Date(dateTimeString);
+
+// Extracting individual components
+const year = dateTime.getFullYear();
+const month = dateTime.getMonth() + 1; // January is month 0
+const day = dateTime.getDate();
+const hours = dateTime.getHours();
+const minutes = dateTime.getMinutes();
+const seconds = dateTime.getSeconds();
+
+// Creating the formatted date and time string
+const formattedDateTime = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}  ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+
+
+
+createdDate.innerText =`Posted At : ${formattedDateTime}` ;
+
+imagediv.append(image)
 card.append(image,title,owner,createdDate)
 
 
