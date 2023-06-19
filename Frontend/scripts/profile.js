b0007645514d13322c80b5b759652eb72ffe7df4
keertiPawar8 <keertipawar8@gmail.com>
2023-06-19 08:53:43 +0530

    dashboard updated

---
+++ Frontend/scripts/profile.js
@@ -0,0 +1,15 @@
+let name = document.querySelector("#name")
+let email = document.querySelector("#email")
+let phone = document.querySelector("#phone")
+let subscriber = document.querySelector("#subscriber")
+
+
+
+
+
+
+
+name.innerText = "keerti"
+email.innerText = "keerti"
+phone.innerText = "keerti"
+subscriber.innerText = "keerti"
